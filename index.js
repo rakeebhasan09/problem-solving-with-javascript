59ddb59f4e89b6bdd0c6bfe219cc9933bc41830a
rakeebhasan <mdrakeebhasan1@gmail.com>
2026-01-07 22:38:59 +0600

    Start today's problem solving with javascript.

---
+++ index.js
@@ -0,0 +1,10 @@
+// Problem One
+function reverseString(str) {
+	let reversed = "";
+	for (let i = str.length - 1; i >= 0; i--) {
+		reversed += str[i];
+	}
+	return reversed;
+}
+
+console.log(reverseString("hello"));
